6f46e31663930944308ca3e8f452285d8038dbbf
Khabib <khabib@MacBook-Pro-Khabib.local>
2024-11-03 17:54:51 +0300

    my-pizza

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './scss/app.scss'
+import Header from './components/Header'
+import Categories from './components/Categories'
+import Sort from './components/Sort'
+import PizzBlock from './components/PizzaBlock'
+
+function App() {
+	return (
+		<div class='wrapper'>
+			<Header />
+			<div class='content'>
+				<div class='container'>
+					<div class='content__top'>
+						<Categories />
+						<Sort />
+					</div>
+					<h2 class='content__title'>Все пиццы</h2>
+					<div class='content__items'>
+						<PizzBlock title='Мексиканская' price='395' />
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default App
