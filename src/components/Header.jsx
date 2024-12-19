37c68d7ef1e34a316705765e21ad82533d49b042
Khabib <khabib@MacBook-Pro-Khabib.local>
2024-12-19 20:48:13 +0300

    10 day Search min 8

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import logoSvg from '../assets/img/pizza-logo.svg'
-
+import Search from './Search'
 
 function Header() {
 	return (
@@ -8,13 +8,14 @@ function Header() {
 			<div className='container'>
 				<Link to='/'>
 					<div className='header__logo'>
-					<img width='38' src={logoSvg} alt='Pizza logo' />
-					<div>
-						<h1>React Pizza</h1>
-						<p>самая вкусная пицца во вселенной</p>
+						<img width='38' src={logoSvg} alt='Pizza logo' />
+						<div>
+							<h1>React Pizza</h1>
+							<p>самая вкусная пицца во вселенной</p>
+						</div>
 					</div>
-				</div>
 				</Link>
+				<Search />
 				<div className='header__cart'>
 					<Link to='/cart' className='button button--cart'>
 						<span>520 ₽</span>
---
+++ src/components/Search/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './Search.module.scss'
+
+const Search = () => {
+	return (
+		<div className={styles.root}>
+			<input className={styles.input} placeholder='Поиск пиццы' />
+		</div>
+	)
+}
+
+export default Search
